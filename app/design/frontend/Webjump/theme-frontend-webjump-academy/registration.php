b7d184bf662480335a06f2ac95dc6aa500f847af
Michel Santos <michel.santos@webjump.com.br>
2022-02-02 09:44:10 -0300

    tema

---
+++ app/design/frontend/Webjump/theme-frontend-webjump-academy/registration.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * PHP version 7
+ *
+ * @author      Webjump Core Team <dev@webjump.com.br>
+ * @copyright   2020 Webjump (http://www.webjump.com.br)
+ * @license     http://www.webjump.com.br  Copyright
+ * @link        http://www.webjump.com.br
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME,
+    "frontend/Webjump/theme-frontend-webjump-academy",
+    __DIR__
+);
+
+
